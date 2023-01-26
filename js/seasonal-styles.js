09269868fc25f1517245d7ef13b098212c2334ab
sabrina <sabrinachhuoy@outlook.com>
2023-01-26 13:48:04 -0800

    Changes

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -1,63 +1,60 @@
-$("document").ready(function () {
-    $('.seasons a').click(function (e) {//find all a tags inside class of seasons
+$(function () {
+
+    $("document").ready(function () {
+      $('.seasons a').click(function (e) {//find all a tags inside class of seasons
         e.preventDefault();//stop default submission
-        var season = $(this).attr("href");//contents of href attribute of this element
-        //alert(season);
-
+        let season = $(this).attr("href");//contents of href attribute of this element
+        // alert(season);
+  
         season = season.toLowerCase();
-        //make the season lower case
-        switch (season){
-            case "spring":
-                $("#logo").attr("src","images/spring.gif");
-                $("#wear").attr("src","images/spring-wear.jpg");
-                $(".heading").text("Bloomin' like spring");
-                $("html").css("background-color", "#2B7129");
-                break;
-            
-
-                case "summer":
-                $("#logo").attr("src","images/summer.gif");
-                $("#wear").attr("src","images/summer-wear.jpg");
-                $(".heading").text("Hot like summer");
-                $("html").css("background-color", "#EBA52B");
-                break;
-
-                case "fall":
-                $("#logo").attr("src","images/fall.gif");
-                $("#wear").attr("src","images/fall-wear.jpg");
-                $(".heading").text("Fallin for PSL");
-                $("html").css("background-color", "#A81124");
-                break;
-
-                case "winter":
-                $("#logo").attr("src","images/winter.gif");
-                $("#wear").attr("src","images/winter-wear.jpg");
-                $(".heading").text("Cold like winter");
-                $("html").css("background-color", "#005393");
-                break;
-
-            
-            default:
-                alert("No case matches")
-
-
+        console.log(season);
+  
+        switch (season) {
+          case "spring":
+            console.log('this is the working season', season)
+            $('#logo').attr("src", 'images/spring.gif')
+            $('#wear').attr("src", 'images/spring-wear.jpg')
+            $('html').css('background-color', '#2B7129');
+            $('#myHeader').text('Spring');
+            break;
+  
+          case "fall":
+            console.log('this is the working season', season)
+            $('#logo').attr("src", 'images/fall.gif')
+            $('#wear').attr("src", 'images/fall-wear.jpg')
+            $('html').css('background-color', '#A81124');
+            $('#myHeader').text('Fall!');
+            break;
+  
+          case "summer":
+            console.log('this is the working season', season)
+            $('#logo').attr("src", 'images/summer.gif')
+            $('#wear').attr("src", 'images/summer-wear.jpg')
+            $('html').css('background-color', '#EBA52B');
+            $('#myHeader').text(' Summer!');
+            break;
+  
+          case "winter":
+            console.log('this is the working season', season)
+            $('#logo').attr("src", 'images/winter.gif')
+            $('#wear').attr("src", 'images/winter-wear.jpg')
+            $('html').css('background-color', '#005393');
+            $('#myHeader').text('Winter');
+            break;
+  
+          default:
+            alert('no season matches')
+            $('#logo').attr("src", 'images/four-seasons.gif')
+            $('#wear').attr("src", 'images/300x400.png')
+            $('html').css('background-color', '#ccc');
+            $('#myHeader').text('Outfitter for All Seasons');
         }
-        $('#dateForm').submit(function(e){//handle form submission event
-            e.preventDefault();//stop default submission
-			var datetext = $('input[name=dateField]').val();//grab data from textbox
-			var formDate = new Date(datetext);//create a date from form input
-			var today = new Date();
-			
-			if(formDate > today)
-			{
-				alert(datetext + " is in the future!");
-			}else{
-				alert(datetext + " is in the past!");
-			}
-
-
+  
+      });
     });
-});
-
-
-
+  
+  });
+  
+  
+  
+  
